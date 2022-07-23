9dfcd9b3a9f5b6cf8bc4b367f49609e2d8bc677f
samdd1 <109146974+samdd1@users.noreply.github.com>
2022-07-23 20:08:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const MenuBtn = document.querySelector('.menu-btn')
+             const navlinks = document.querySelector('.nav-links')
+
+             MenuBtn.addEventListener('click',()=>{
+                navlinks.classList.toggle('mobile-menu')
+            })
+           
+            
